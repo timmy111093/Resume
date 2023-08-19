b24d58effcd848f296ff8108c4a1fb50239d8569
תמיר אלמקייס <tamiri7252@gmail.com>
2023-08-20 01:11:40 +0300

    index.html changes

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const rootName = "/home";
+// const rootName = "/home";
 root.render(
   // <React.StrictMode>
       <BrowserRouter>
